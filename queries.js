2067635ac17e15cd44eba733d287994e6d92c916
Akshay Chandwani <akshaychandwani992@gmail.com>
2020-03-30 12:18:40 +0530

    modified api according to postgres

--- queries.js
+++ queries.js
@@ -1,14 +1,14 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'me',
+    user: 'postgres',
     host: 'localhost',
-    database: 'api',
-    password: 'password',
+    database: 'sensai',
+    password: 'pULSAR@220',
     port: 5432
 })
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    pool.query('SELECT * FROM users_users ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
@@ -16,21 +16,22 @@ const getUsers = (request, response) => {
     })
 }
 
-const getUserById = (request, response) => {
-    const id = parseInt(request.params.id)
+// const getUserById = (request, response) => {
+//     const id = parseInt(request.params.id)
 
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, response) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+//     pool.query('SELECT * FROM users_users WHERE id = $1', [id], (error, response) => {
+//         if (error) {
+//             throw error
+//         }
+//         response.status(200).json(results.rows)
+//     })
+// }
 
 const createUser = (request, response) => {
-    const { name, email } = request.body
-
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    const { password,email, isActive, isAdmin, isStaff, fullName  } = request.body
+    const now = new Date();
+    pool.query('INSERT INTO users_users (password, last_login, email, is_active, is_admin, is_staff, full_name) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+    [password, now, email, isActive, isAdmin, isStaff, fullName], (error, results) => {
         if (error) {
             throw error
         }
@@ -38,37 +39,37 @@ const createUser = (request, response) => {
     })
 }
 
-const updateUser = (request, response) => {
-    const id = parseInt(request.params.id);
-    const {name, email} = request.body;
+// const updateUser = (request, response) => {
+//     const id = parseInt(request.params.id);
+//     const {name, email} = request.body;
 
-    pool.query(
-        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-        [name, email, id],
-        (error, results) => {
-            if(error) {
-                throw error;
-            }
-            response.status(200).send(`User modified with IDL ${id}`)
-        }
-    )
-}
+//     pool.query(
+//         'UPDATE users_users SET name = $1, email = $2 WHERE id = $3',
+//         [name, email, id],
+//         (error, results) => {
+//             if(error) {
+//                 throw error;
+//             }
+//             response.status(200).send(`User modified with IDL ${id}`)
+//         }
+//     )
+// }
 
-const deleteUser = (request, response) => {
-    const id = parseInt(request.params.id);
+// const deleteUser = (request, response) => {
+//     const id = parseInt(request.params.id);
 
-    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-        if(error) {
-            throw error;
-        }
-        response.status(200).send(`User deleted with ID: ${id}`);
-    })
-}
+//     pool.query('DELETE FROM users_users WHERE id = $1', [id], (error, results) => {
+//         if(error) {
+//             throw error;
+//         }
+//         response.status(200).send(`User deleted with ID: ${id}`);
+//     })
+// }
 
 module.exports = {
-    getUsers,
-    getUserById,
+     getUsers,
+    // getUserById,
     createUser,
-    updateUser,
-    deleteUser
+    // updateUser,
+    // deleteUser
 }
